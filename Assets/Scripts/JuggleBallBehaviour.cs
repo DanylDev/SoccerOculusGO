7c85703931f92549908477e6139fa2483d5b1214
DanylUnity3D <devandrienko@gmail.com>
2018-08-20 17:55:45 +0300

    Ball slightly, randomly changes its position, when contacted with head. There's even no need for 3-hit timeout mechanics (which is kinda odd in real life) - gameplay is always intense | Ball is always on same height, during gaming process. No "higher and higher" bug anymore | Realistic bounciness on grass

--- Assets/Scripts/JuggleBallBehaviour.cs
+++ Assets/Scripts/JuggleBallBehaviour.cs
@@ -10,12 +10,19 @@ public class JuggleBallBehaviour : MonoBehaviour
     
     private bool isLost;
 
+    private Rigidbody _rb;
+    
+    private void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.name == playerHeadGO.name && !isLost)
         {
             //+1 to score
-            Score.CurrentScoreJuggle++;
+            //Score.CurrentScoreJuggle++;
         }
         else if (other.name == floorGO.name)
         {
@@ -24,4 +31,18 @@ public class JuggleBallBehaviour : MonoBehaviour
             isLost = true;
         }
     }
+    
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == playerHeadGO.name)
+        {
+            GetComponent<Rigidbody>().velocity = new Vector3(_rb.velocity.x+Random.Range(-0.1f, 0.1f), 5, _rb.velocity.z+Random.Range(-0.1f, 0.1f));
+            
+            //+1 to score
+            if (!isLost)
+            {
+                Score.CurrentScoreJuggle++;
+            }
+        }
+    } 
 }
